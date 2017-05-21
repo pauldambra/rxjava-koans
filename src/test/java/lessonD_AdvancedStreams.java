c4cd64fa8cfe06382c96ae5312c8e6f4855c651a
pauldambra <pauldambra@gmail.com>
2017-05-21 12:29:54 +0100

    complete lesson d

--- src/test/java/lessonD_AdvancedStreams.java
+++ src/test/java/lessonD_AdvancedStreams.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import rx.Observable;
 import rx.functions.Func1;
 import rx.observables.GroupedObservable;
+import rx.observables.MathObservable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,7 +30,7 @@ public class lessonD_AdvancedStreams {
 
         you.mergeWith(me).subscribe(string -> mReceived += string + " ");
 
-        assertThat(mReceived).isEqualTo(_____);
+        assertThat(mReceived).isEqualTo("1 2 3 A B C ");
     }
 
     /*
@@ -44,10 +45,7 @@ public class lessonD_AdvancedStreams {
     @Test
     public void _2_splittingUp() {
         Observable.range(1, 9)
-                .groupBy(integer -> {
-                    // ____
-                    return _____;
-                })
+                .groupBy(integer -> integer % 2 == 0 ? "even" : "odd")
                 .subscribe(group -> group.subscribe(integer -> {
                     String key = group.getKey();
                     if ("even".equals(key)) {
@@ -79,7 +77,7 @@ public class lessonD_AdvancedStreams {
                 group -> {
                     Observable<Double> convertToDouble = group.map(integer -> (double) integer);
                     Func1<Double, Double> insertIntoAveragesArray = aDouble -> averages[group.getKey()] = aDouble;
-//                  MathObservable.averageDouble(________).map(____________).______();
+                    MathObservable.averageDouble(convertToDouble).map(insertIntoAveragesArray).subscribe();
                 }
         );
 
